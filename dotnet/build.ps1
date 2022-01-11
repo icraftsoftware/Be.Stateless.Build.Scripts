619fefc550d4b0549af9c39c5204f2d2ed30bcad
François Chabot <francois.chabot@stateless.be>
2022-01-11 13:52:24 +0100

    Honor build props major and minor for dotnet

--- dotnet/build.ps1
+++ dotnet/build.ps1
@@ -1,6 +1,6 @@
 ﻿#region Copyright & License
 
-# Copyright © 2012 - 2020 François Chabot
+# Copyright © 2012 - 2022 François Chabot
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,39 +19,51 @@
 [CmdletBinding()]
 [OutputType([void])]
 param(
-    [Parameter(Mandatory = $false)]
-    [int]
-    $Major = 2,
+   [Parameter(Mandatory = $false)]
+   [int]
+   $Major = 0,
 
-    [Parameter(Mandatory = $false)]
-    [int]
-    $Minor = 0,
+   [Parameter(Mandatory = $false)]
+   [int]
+   $Minor = 0,
 
-    [Parameter(Mandatory = $false)]
-    [int]
-    $Build = ('{0:yy}{1:000}' -f [datetime]::Today, [datetime]::Today.DayOfYear),
+   [Parameter(Mandatory = $false)]
+   [int]
+   $Build = ('{0:yy}{1:000}' -f [datetime]::Today, [datetime]::Today.DayOfYear),
 
-    [Parameter(Mandatory = $false)]
-    [int]
-    $Revision = (([datetime]::Now - [datetime]::Today).TotalSeconds / 1.4)
+   [Parameter(Mandatory = $false)]
+   [int]
+   $Revision = (([datetime]::Now - [datetime]::Today).TotalSeconds / 1.4)
 )
 
 # construct a Version object to ensure arguments are valid
+if ($Major -eq 0) {
+   $defaultVersion = ([xml](Get-Content .\src\Directory.Build.props)).Project.PropertyGroup
+   if ($defaultVersion.Major -gt 0) {
+      $Major = [math]::max(2, $defaultVersion.Major)
+      $Minor = [math]::max(0, $defaultVersion.Minor)
+   } else {
+      $defaultVersion = ([xml](Get-Content $PSScriptRoot\..\Directory.Build.props)).Project.PropertyGroup
+      if ($defaultVersion.Major.InnerText -gt 0) {
+         $Major = [math]::max(2, $defaultVersion.Major.InnerText)
+         $Minor = [math]::max(0, $defaultVersion.Minor.InnerText)
+      }
+   }
+}
 $version = New-Object -TypeName System.Version -ArgumentList $Major, $Minor, $Build, $Revision
 
 # build and package solution
-if (Get-ChildItem -Path *.sln | Where-Object Name -match 'Be\.Stateless\.BizTalk\.(Activity\.Tracking|Dummies|Orchestrations|Pipelines|Schemas|Transforms)\.sln') {
-    MSBuild.exe /Target:restore
-    MSBuild.exe /property:DelaySign=false`;Configuration=Debug`;Major=$($version.Major)`;Minor=$($Version.Minor)`;Build=$($Version.Build)`;Revision=$($version.Revision)
-    MSBuild.exe /property:DelaySign=false`;Configuration=Release`;Major=$($version.Major)`;Minor=$($Version.Minor)`;Build=$($Version.Build)`;Revision=$($version.Revision)`;GeneratePackageOnBuild=true`;NoWarn=1591
-}
-else {
-    dotnet restore
-    dotnet build -p:DelaySign=false`;Configuration=Debug`;Major=$($version.Major)`;Minor=$($Version.Minor)`;Build=$($Version.Build)`;Revision=$($version.Revision)
-    dotnet build -p:DelaySign=false`;Configuration=Release`;Major=$($version.Major)`;Minor=$($Version.Minor)`;Build=$($Version.Build)`;Revision=$($version.Revision)`;GeneratePackageOnBuild=true`;NoWarn=1591
+if (Get-ChildItem -Path *.sln | Where-Object Name -Match 'Be\.Stateless\.BizTalk\.(Activity\.Tracking|Dummies|Orchestrations|Pipelines|Schemas|Transforms)\.sln') {
+   MSBuild.exe /Target:restore
+   MSBuild.exe /property:DelaySign=false`;Configuration=Debug`;Major=$($version.Major)`;Minor=$($Version.Minor)`;Build=$($Version.Build)`;Revision=$($version.Revision)
+   MSBuild.exe /property:DelaySign=false`;Configuration=Release`;Major=$($version.Major)`;Minor=$($Version.Minor)`;Build=$($Version.Build)`;Revision=$($version.Revision)`;GeneratePackageOnBuild=true`;NoWarn=1591
+} else {
+   dotnet restore
+   dotnet build -p:DelaySign=false`;Configuration=Debug`;Major=$($version.Major)`;Minor=$($Version.Minor)`;Build=$($Version.Build)`;Revision=$($version.Revision)
+   dotnet build -p:DelaySign=false`;Configuration=Release`;Major=$($version.Major)`;Minor=$($Version.Minor)`;Build=$($Version.Build)`;Revision=$($version.Revision)`;GeneratePackageOnBuild=true`;NoWarn=1591
 }
 
-# generate build.local.ps1 script file that allows to redo a build and package locally wihtout altering the build version number
+# generate build.local.ps1 script file that allows to redo a build and package locally without altering the build version number
 $path = Split-Path $script:MyInvocation.MyCommand.Path
 @"
 & $(Join-Path $path build.ps1) -Major $($version.Major) -Minor $($version.Minor) -Build $($version.Build) -Revision $($version.Revision)
